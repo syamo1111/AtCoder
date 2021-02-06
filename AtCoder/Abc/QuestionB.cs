d9658e96f1936cd4e2c4c0d7510a4b9db4c7392a
syamo1111 <mtsza.mtznh@gmail.com>
2021-02-06 21:45:11 +0900

    BがAC

--- AtCoder/Abc/QuestionB.cs
+++ AtCoder/Abc/QuestionB.cs
@@ -13,24 +13,19 @@ namespace AtCoder.Abc
             var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
             Console.SetOut(sw);
 
-            // 文字列の入力
-            string s = Console.ReadLine();
-
-            // 整数の入力
-            long n = long.Parse(Console.ReadLine());
-
-            // 文字列配列の入力
-            string[] inputStrArray = Console.ReadLine().Split(' ');
-
             // 整数配列の入力
             var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
 
+            var N = inputLongArray[0];
+            long X = inputLongArray[1];
+
+            var input = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+            var resultArray = input.Where(i => i != X);
 
 
-
-            string result = "";
-
-            Console.WriteLine(result);
+            Console.WriteLine(string.Join(" ",resultArray));
+            
 
             Console.Out.Flush();
         }
